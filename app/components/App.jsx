64f234217d3998b37667e706f46deffcab573ed6
TJ Egan <twegan@us.ibm.com>
2016-06-21 17:14:08 -0500

    add ability to edit note inline

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,5 +1,6 @@
 import uuid from 'node-uuid';
 import React from 'react';
+import Notes from './Notes.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -26,10 +27,14 @@ export default class App extends React.Component {
     const notes = this.state.notes;    
     return (
       <div>
-        <ul>{notes.map(note =>
-          <li key={note.id}>{note.task}</li>
-        )}</ul>
+        <button onClick={this.addNote}>+</button>
+        <Notes notes={notes} />
       </div>
-    )
+    );
+  }
+  addNote = () => {
+    this.setState({
+      notes: [...this.state.notes, {id: uuid.v4(), task: 'New Task'}]
+    })
   }
 }
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,57 @@
 import React from 'react';
 
-export default() => <div>Learn Webpack!!</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+    
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text" 
+      ref={
+        (e) => e ? e.selectionStart = this.props.task.length : null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />
+
+  };
+  renderNote = () => {
+    // If the user clicks a normal note, trigger editing logic
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+  edit = () => {
+    // Enter edit mode
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    // The user hit "enter"
+    if(e.key === 'Enter') {
+      this.finishEdit(e)
+    }
+  };
+  finishEdit = (e) => {
+    const value = e.target.value;
+    
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+      
+      // Exit edit mode
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
